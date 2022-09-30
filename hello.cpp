f8bd671d8a29569ea3a55046e8196d5fbd97326e
avsavushkiny <avsavushkiny@live.ru>
2022-10-01 00:44:38 +0300

    add hello

---
+++ hello.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+string text;
+int a;
+float b;
+
+int m[5] = {10,11,12,13,14};
+
+int main(void)
+{
+
+    for (int aa = 0; aa <= 4; aa++)
+    {
+        cout << m[aa] << endl;
+    }
+
+
+    /*cout << "create Name" << endl;
+    cin >> text;
+    cout << text << endl;*/
+
+    system("pause");
+
+    return 0;
+}
