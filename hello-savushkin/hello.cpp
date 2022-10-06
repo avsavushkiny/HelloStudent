5d1f15000b3c9b88d39e124734b96490b9529482
avsavushkiny <avsavushkiny@live.ru>
2022-10-07 00:33:40 +0300

    add massiv

--- hello-savushkin/hello.cpp
+++ hello-savushkin/hello.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 string text;
 
+//int myMassiv[3]{}; /*0 1 2*/
+
 int main(void)
 {
 
     cout << "you Name?" << endl;
     cin  >> text;
     cout << "hello " << text << endl;
+
+    int myMassiv[3] = {11, 22, 33};
+
+    cout << myMassiv[4] << endl;
 
     return 0;
 }
