bcfa011f2b1a297eeb5b876c6e0d248ea45d71cc
avsavushkiny <avsavushkiny@live.ru>
2022-10-07 21:25:09 +0300

    add

--- hello-savushkin/hello.cpp
+++ hello-savushkin/hello.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
 string text;
 
-//int myMassiv[3]{}; /*0 1 2*/
-
 int main(void)
 {
-
+    /*
     cout << "you Name?" << endl;
     cin  >> text;
     cout << "hello " << text << endl;
@@ -15,6 +15,14 @@ int main(void)
     int myMassiv[3] = {11, 22, 33};
 
     cout << myMassiv[4] << endl;
+    */
+
+    for (;;)
+    {
+        int a = rand() % 10 + 1;
+
+        cout << a << endl;
+    }
 
     return 0;
 }
