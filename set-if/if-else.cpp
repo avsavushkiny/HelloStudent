424905d1959ef6267d1dc0a404b2456441e7d3db
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2022-11-11 12:29:19 +0300

    Create if-else.cpp

---
+++ set-if/if-else.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "Repeat text" << endl;
+
+    cin.get(ch);
+
+    while (ch != '.')
+    {
+        if (ch == '\n')
+        {
+            cout << ch;
+        }
+        else cout << +ch;
+        
+        cin.get(ch);
+    }
+    return 0;
+}
